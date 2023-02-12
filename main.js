9b4a658557433efac1e258b16e5a1c1531813f03
Ervez <fb.blaszkiewicz@gmail.com>
2023-02-12 22:23:29 +0100

    desktop done?

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(function(){
     const ratingButtons = [...$(".ratingButton")];
+    let isRatingClicked = false;
     ratingButtons.forEach(button =>{
         button.addEventListener('click', raingClicked);
     })
@@ -8,5 +9,25 @@ $(function(){
         $('.ratingButton').attr('class', 'ratingButton');
         let currentRatingSelected = event.target;
         $(currentRatingSelected).addClass('ratingButtonSelected');
+        isRatingClicked = true;
     }
+
+    $( ".submitButton" ).click(function() {
+        if(isRatingClicked == false){
+            shakeElement(this);
+        }else{
+            $('.containerMain').fadeOut();
+            setTimeout(() => {
+                $('.containerThankState').fadeIn();
+              }, 500)
+        }
+      });
+
+
+      function shakeElement(element){
+        $(element).addClass('shake');
+        setTimeout(function(){
+            $(element).removeClass('shake');
+        },1000);
+    };
 })
